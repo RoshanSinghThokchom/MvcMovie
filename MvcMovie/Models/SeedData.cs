ff51a7fbb2c64711ec50a0c1362257e3b12bea80
ThokchomRoshanSingh <thokchomroshan64@gmail.com>
2025-05-30 21:05:01 -0400

    Final Commit

--- MvcMovie/Models/SeedData.cs
+++ MvcMovie/Models/SeedData.cs
@@ -22,35 +22,43 @@ public static class SeedData
             context.Movie.AddRange(
                 new Movie
                 {
-                    Title = "When Harry Met Sally",
-                    ReleaseDate = DateTime.Parse("1989-2-12"),
-                    Genre = "Romantic Comedy",
+                    Title = "The Shawshank Redemption",
+                    ReleaseDate = DateTime.Parse("1994-2-2"),
+                    Genre = "Thriller/Crime",
                     Rating = "R",
-                    Price = 7.99M
+                    Price = 9.4M
                 },
                 new Movie
                 {
-                    Title = "Ghostbusters ",
-                    ReleaseDate = DateTime.Parse("1984-3-13"),
-                    Genre = "Comedy",
+                    Title = "The Godfather ",
+                    ReleaseDate = DateTime.Parse("1972-5-21"),
+                    Genre = "Crime",
                     Rating = "R",
-                    Price = 8.99M
+                    Price = 7.79M
                 },
                 new Movie
                 {
-                    Title = "Ghostbusters 2",
-                    ReleaseDate = DateTime.Parse("1986-2-23"),
-                    Genre = "Comedy",
+                    Title = "Forrest Gump",
+                    ReleaseDate = DateTime.Parse("1994-1-3"),
+                    Genre = "Comedy/Romance",
                     Rating = "R",
                     Price = 9.99M
                 },
                 new Movie
                 {
-                    Title = "Rio Bravo",
-                    ReleaseDate = DateTime.Parse("1959-4-15"),
-                    Genre = "Western",
+                    Title = "Fight Club",
+                    ReleaseDate = DateTime.Parse("1999-1-8"),
+                    Genre = "Action/Crime",
                     Rating = "R",
-                    Price = 3.99M
+                    Price = 9.8M
+                },
+                new Movie
+                {
+                    Title = "Joker",
+                    ReleaseDate = DateTime.Parse("2019-4-15"),
+                    Genre = "Thriller/Crime",
+                    Rating = "R",
+                    Price = 6.99M
                 }
             );
             context.SaveChanges();
